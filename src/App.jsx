1b0fefaa0039235f3bae80429cbd0d7cd3fa970e
Daiana <dauipoma@gmail.com>
2023-03-09 21:47:35 +0100

    refactor: add function for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from 'react'
-import Advice from './components/Advice'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
   const [advice, setAdvice] = useState([])
 
-  useEffect(()=> {
+  const fetchAdvice = () => {
     fetch('	https://api.adviceslip.com/advice')
     .then(res => res.json())
     .then(({slip}) => {
       const {id, advice} = slip
       setAdvice([id, advice])
     })
-  }, [])
+  }
+
+  useEffect(fetchAdvice, [])
 
   return (
     <div className="App">
-      <Advice id={advice[0]} advice={advice[1]}/>
+      <Card id={advice[0]} advice={advice[1]} changeAdvice={fetchAdvice}/>
     </div>
   )
 }
