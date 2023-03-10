511ad647ed8026f0d5976dfa5b5262e6312b9ee0
Daiana <dauipoma@gmail.com>
2023-03-10 08:15:20 +0100

    refactor: add divider to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Button from './Button'
 
 export default function Card({id, advice, changeAdvice}) {
-
   return (
     <div className='card'>
       <span className='card-id'>Advice #{id}</span>
-      <p className='card-advice'>"{advice}"</p>
+      <p className='card-advice'><span className='quotes'>&quot;</span>{advice}<span className='quotes'>&quot;</span></p>
+      <div className="divider">
+      </div>
       <Button changeAdvice={changeAdvice}/>
     </div>
   )
